7132a62dba558e9154d02c1891569c8d3eb24458
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2023-11-02 08:09:25 -0700

    Update unit1_MadLibs.py

--- TechSmart/unit1_MadLibs.py
+++ TechSmart/unit1_MadLibs.py
@@ -1,26 +1,24 @@
-"""
-Unit 1
-Code Your Own: Unit 1
-[Madlib]
-"""
-
 # Print instructions
 print("Let's write a Madlib together!\nA madlib is a short story where you fill in the blanks.\n")
 
-# First sentence
-madlib = "Today I went to my favorite Taco Stand called the " + input("Enter a one-syllable adjective: ") + " " + input("Enter a plural Food: ") + "."
+# Ask for inputs
+adjective = input("Enter a one-syllable adjective: ")
+food_plural = input("Enter a plural food: ")
+vehicle = input("Enter something you would ride in: ")
+verb = input("Enter a verb: ")
+color = input("Enter a color: ")
+noun = input("Enter a noun: ")
+food_plural2 = input("Enter a plural food: ")
+food_plural3 = input("Enter a plural food: ")
+person = input("Enter a person: ")
+saying = input("Enter a saying: ")
 
-# Second sentence
-madlib += "Unlike most food stands, they cook and prepare the food in a " + input("Enter something you would ride in: ") + " while you " + input("Enter a verb: ") + " . "
-
-# Third sentence
-madlib += "The best thing on the menu is the " + input("Enter a color: ") + " " + input("Enter a noun: ") + ". "
-
-# Fourth sentence
-madlib += "Instead of ground beef they fill the taco with " + input("Enter a plural food: ") + ", cheese, and top it off with a salsa made from " + input("Enter a plural food: ") + ". "
-
-# Fifth sentence
-madlib += "If that doesn't make your mouth water, then it' just like " + input("Enter a Person: ") + " always says: " + input("Enter a saying: ") + "!\n"
+# Create the Madlib story
+madlib = "Today I went to my favorite Taco Stand called the " + adjective + " " + food_plural + "."
+madlib += " Unlike most food stands, they cook and prepare the food in a " + vehicle + " while you " + verb + ". "
+madlib += "The best thing on the menu is the " + color + " " + noun + ". "
+madlib += "Instead of ground beef, they fill the taco with " + food_plural2 + ", cheese, and top it off with a salsa made from " + food_plural3 + ". "
+madlib += "If that doesn't make your mouth water, then it's just like " + person + " always says: " + saying + "!\n"
 
 # Print Madlib
 print(madlib)
