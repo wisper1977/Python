69f0af56a4a94941e96181b0486103dace2c7c2a
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2023-12-14 07:34:52 -0700

    Update Unit6_Password_checker.py

--- TechSmart/Unit6_Password_checker.py
+++ TechSmart/Unit6_Password_checker.py
@@ -1,7 +1,6 @@
 """
-Unit6_Password_checker
-
-Description:
+Do not begin coding.
+Instead, fill out the Planning worksheet.
 """
 
 def check_password_strength(password, min_length=8):
@@ -20,9 +19,13 @@ def check_password_strength(password, min_length=8):
 
     # Check for additional criteria (e.g., uppercase, lowercase, digits)
     criteria_met = check_additional_criteria(password)
+    special_char_met = check_special_characters(password)
     
     if not criteria_met:
         return "Weak password. Please include uppercase letters, lowercase letters, and digits."
+    
+    if not special_char_met:
+        return "Weak password. Please include special characters."
 
     return "Strong password. Good job!"
 
@@ -41,6 +44,19 @@ def check_additional_criteria(password):
     has_digit = any(char.isdigit() for char in password)
 
     return has_uppercase and has_lowercase and has_digit
+
+def check_special_characters(password):
+    """
+    Check if the password contains special characters.
+
+    Parameters:
+    - password (str): The password to be checked.
+
+    Returns:
+    - bool: True if special characters are present, False otherwise.
+    """
+    special_characters = "!@#$%^&*()_-+=<>?/{}[]|"
+    return any(char in special_characters for char in password)
 
 password = input("Enter your password: ")
 min_length = int(input("Enter the minimum password length (press Enter for default): ") or 8)
