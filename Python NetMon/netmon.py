b9c2c8a4151bb76f8e563b704ca5e475994d0cea
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2024-05-22 06:17:48 -0700

    Add files via upload

--- Python NetMon/netmon.py
+++ Python NetMon/netmon.py
@@ -5,12 +5,15 @@
 import tkinter as tk
 from modules.log_manager import LogManager
 from modules.application import Application
+from modules.config_manager import ConfigManager
 
 if __name__ == "__main__":
     """Main entry point for the application."""
-    log_manager = LogManager.get_instance()
+    config_manager = ConfigManager()
+    log_manager = LogManager.get_instance(config_manager)
+    config_manager.set_logger(log_manager)
     log_manager.log_info("Application started")
     root = tk.Tk()
-    app = Application(master=root)
+    app = Application(master=root, config_manager=config_manager, log_manager=log_manager)
     app.mainloop()
     log_manager.log_info("Application ended")
