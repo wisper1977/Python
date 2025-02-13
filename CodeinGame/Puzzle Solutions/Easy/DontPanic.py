165f8f7d36aecc56bd85274dc73d3257bba30402
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2025-02-13 13:22:18 -0700

    Update DontPanic.py

--- CodeinGame/Puzzle Solutions/Easy/DontPanic.py
+++ CodeinGame/Puzzle Solutions/Easy/DontPanic.py
@@ -1,32 +1,19 @@
+import sys
+
 # Read initialization inputs
-nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = map(int, input().split())
+data = list(map(int, input().split()))
+nb_floors, width, nb_rounds, exit_floor, exit_pos, _, _, nb_elevators = data
 
 # Dictionary to store elevator positions per floor
-elevators = {}
-for _ in range(nb_elevators):
-    floor, pos = map(int, input().split())
-    elevators[floor] = pos
-
-# Add exit position as an "elevator" for logic simplicity
-elevators[exit_floor] = exit_pos
+elevators = {exit_floor: exit_pos, **{floor: pos for floor, pos in (map(int, input().split()) for _ in range(nb_elevators))}}
 
 while True:
-    # Read the current state of the leading clone
-    inputs = input().split()
-    clone_floor = int(inputs[0])
-    clone_pos = int(inputs[1])
-    direction = inputs[2]
+    clone_floor, clone_pos, direction = input().split()
+    clone_floor, clone_pos = int(clone_floor), int(clone_pos)
     
-    # If no active clone, wait
     if clone_floor == -1:
         print("WAIT")
         continue
     
-    # Get the target position (exit or elevator on this floor)
     target_pos = elevators.get(clone_floor, exit_pos)
-    
-    # Determine whether to block or wait
-    if (clone_pos < target_pos and direction == "LEFT") or (clone_pos > target_pos and direction == "RIGHT"):
-        print("BLOCK")
-    else:
-        print("WAIT")
+    print("BLOCK" if (clone_pos < target_pos and direction == "LEFT") or (clone_pos > target_pos and direction == "RIGHT") else "WAIT")
