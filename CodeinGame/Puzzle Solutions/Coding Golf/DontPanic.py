ad398be9c3eab7b437e54654953f9a4b26795d71
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2025-02-13 13:32:32 -0700

    Update DontPanic.py

--- CodeinGame/Puzzle Solutions/Coding Golf/DontPanic.py
+++ CodeinGame/Puzzle Solutions/Coding Golf/DontPanic.py
@@ -1,4 +1,4 @@
 I,N=lambda:input().split(),int
 *_,y,x,_,_,n=map(N,I())
 e={N(f):N(p)for f,p in(I()for _ in range(n))}|{y:x}
-while 1:j,k,d=I();j,k=N(j),N(k);print("BLOCK"if(k<(f:=e.get(j,y))and d<"M")or(k>f and d>"M")else"WAIT")
+while 1:j,k,d=I();print("BLOCK"if(N(k)<(f:=e.get(N(j),y))and d<"M")or(N(k)>f and d>"M")else"WAIT")
